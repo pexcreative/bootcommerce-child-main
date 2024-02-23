34c5cf106b6354989337566c8e25ae30afbb9d7e
pexcreative <45134331+pexcreative@users.noreply.github.com>
2024-02-23 15:44:09 -0300

    actualizacion

---
+++ img/crear favicon.bat
@@ -0,0 +1,7 @@
+copy ORIGINAL.png android-chrome-192x192.png
+copy ORIGINAL.png android-chrome-512x512.png
+copy ORIGINAL.png apple-touch-icon.png
+copy ORIGINAL.png favicon.png
+copy ORIGINAL.png favicon-16x16.png
+copy ORIGINAL.png favicon-32x32.png
+copy ORIGINAL.png mstile-150x150.png
